9714afb33aa183462f558fc3d28a60cf4ce89e0a
IanHinden <Ian.Hinden@gmail.com>
2015-07-08 22:30:40 -0700

    Browser Object Model Test

--- Javascript/test-function.js
+++ Javascript/test-function.js
@@ -4,3 +4,13 @@ function updateMessage() {
 	el.textContent = msg;
 }
 updateMessage();
+
+var msg2 = '<h2>browser window</h2><p>width: ' + window.innerWidth + '</p>';
+msg2 += '<p>height: ' + window.innerHeight + '</p>';
+msg2 += '<h2>history</h2><p>items: ' + window.history.length + '</p>';
+msg2 += '<h2>screen</h2><p>width: ' + window.screen.width + '</p>';
+msg2 += '<p>height: ' + window.screen.height + '</p>';
+
+var el2 = document.getElementById('info');
+el2.innerHTML = msg2;
+alert('Current page: ' + window.location);
